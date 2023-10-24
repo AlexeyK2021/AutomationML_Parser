f6198a1a17a7d3c469ce8b9a4d8257d847a6e56a
Alexey Kalashnikov <kalashnickov.alexey2015@yandex.ru>
2023-10-24 21:02:37 +0300

    Transfering to C#

---
+++ aml/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Aml.Engine.CAEX;
+using static System.Console;
+
+class Hello
+{
+    public static void Main()
+    {
+        var document = CAEXDocument.LoadFromFile("..\\..\\..\\TestAML.aml");
+        var elements = document.CAEXFile.InstanceHierarchy.Elements;
+        foreach(var el in elements){
+            WriteLine(el.ToString());
+        }
+    }
+}
