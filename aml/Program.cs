c43351c152e36cb82e027c96a1571e77f157406d
Alexey Kalashnikov <kalashnickov.alexey2015@yandex.ru>
2023-10-24 22:38:45 +0300

    testing AML lib

--- aml/Program.cs
+++ aml/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Aml.Engine.CAEX;
+using Aml.Engine.CAEX.Extensions;
 using static System.Console;
 
 class Hello
@@ -7,9 +8,28 @@ class Hello
     public static void Main()
     {
         var document = CAEXDocument.LoadFromFile("..\\..\\..\\TestAML.aml");
-        var elements = document.CAEXFile.InstanceHierarchy.Elements;
-        foreach(var el in elements){
-            WriteLine(el.ToString());
+        var elements = document.CAEXFile.InstanceHierarchy;
+
+        // foreach (var el in elements)
+        // {
+        //     WriteLine(el.CAEXChild("InternalElement").CAEXChild("InternalElement").CAEXChild("ExternalInterface").CAEXChild("ExternalInterface").Name());
+        // }
+
+
+        foreach (var instanceHierarchy in document.CAEXFile.SystemUnitClassLib)
+        {
+            foreach (var internalElement in instanceHierarchy.Descendants<SystemUnitClassType>())
+            {
+                WriteLine(internalElement.Name);
+            }
+        }
+    }
+
+    private static void printSome(List<CAEXWrapper> Element)
+    {
+        foreach (var el in Element)
+        {
+            WriteLine(el.Name());
         }
     }
 }
